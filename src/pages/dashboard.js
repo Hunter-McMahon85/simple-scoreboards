efd20d9d293f90b092140a7f95e842f26456f474
Deihman <clownvant@icloud.com>
2023-10-28 13:13:25 -0700

    cool buttons

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -25,16 +25,30 @@ const Dashboard = () => {
   return (
     <>
       <div className="dash_body">
+        <h1 style={{textAlign: 'center'}}>Welcome to the dashboard!</h1>
+        
         <Link to="/">
-          <button onClick={signOut}>Sign Out</button>
-        </Link>
-        <Link to="/presetcreator">
-          <button>Create Scoreboard Preset</button>
+          <button 
+          onClick={signOut} 
+          style={{color: 'white', backgroundColor: 'red', padding: '6px 20px', borderRadius: '20px',
+                  cursor: 'pointer', float: 'right'}}>
+            Sign Out
+          </button>
         </Link>
         
-        <button onClick={handleopenSessionStart}>
+        <Link to="/presetcreator">
+          <button
+          style={{color: 'black', backgroundColor: 'lightblue', padding: '6px 20px', borderRadius: '20px',
+          cursor: 'pointer', display: "block"}}>
+            Create Scoreboard Preset</button>
+        </Link>
+        
+        <button onClick={handleopenSessionStart}
+        style={{color: 'black', backgroundColor: 'lightgreen', padding: '6px 20px', borderRadius: '20px',
+        cursor: 'pointer'}}>
           Start Scorekeeping Session
         </button>
+
         {ShowSessionStart && <SessionStart onClose={handleCloseSessionStart} />}
       </div>
     </>
