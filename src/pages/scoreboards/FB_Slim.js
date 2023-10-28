9049f77eb8bd7e421f8321e7eaaed5487b478789
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2023-10-27 23:55:02 -0700

    added basic stlye to demo

--- src/pages/scoreboards/FB_Slim.js
+++ src/pages/scoreboards/FB_Slim.js
@@ -1,19 +1,23 @@
 import React from "react";
+import "../../css/FB_Slim.css";
 
 const FBSlim = ({ V_score, H_score, Period, Time, Down, Distance }) => {
   return (
     <>
-      <img alt="Broadcaster Logo"></img>
-      <h1>Visitor Score: {V_score}</h1>
-      <h1>Home Score: {H_score}</h1>
-      <h1>Quarter {Period}</h1>
-      <h1>{Time}</h1>
-      <h1>{Down} & {Distance}</h1>
+      <div className="FB_Slim_Container">
+        <div className="FB_Slim_items">
+          <img alt="Broadcaster Logo"></img>
+          <h1> Home Score: {H_score} </h1>
+          <h1> Visitor Score: {V_score} </h1>
+          <h1> Quarter {Period} </h1>
+          <h1> {Time} </h1>
+          <h1>
+            {Down} & {Distance}
+          </h1>
+        </div>
+      </div>
     </>
   );
 };
 
 export default FBSlim;
-
-
-
