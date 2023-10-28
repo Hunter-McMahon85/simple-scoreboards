aadc9a7ccdc79b798440cfc2a5c89bf0d060ffbc
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2023-10-28 00:04:40 -0700

    amplify being difficult with new scoreboard page

--- src/pages/scoreboards/FB_Slim.js
+++ src/pages/scoreboards/FB_Slim.js
@@ -10,7 +10,7 @@ const FBSlim = ({ V_score, H_score, Period, Time, Down, Distance }) => {
           <h1> Home Score: {H_score} </h1>
           <h1> Visitor Score: {V_score} </h1>
           <h1> Quarter {Period} </h1>
-          <h1> {Time} </h1>
+          <h1> {Time} | </h1>
           <h1>
             {Down} & {Distance}
           </h1>
