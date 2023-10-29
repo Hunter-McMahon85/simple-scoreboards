d2538054d47081e5184190d441a9f9003813b0c3
Deihman <clownvant@icloud.com>
2023-10-28 17:32:23 -0700

    placeholder saved preset delay added

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -30,7 +30,7 @@ const Dashboard = () => {
         <Link to="/">
           <button 
           onClick={signOut} 
-          style={{color: 'white', backgroundColor: 'red', padding: '6px 20px', borderRadius: '20px',
+          style={{color: 'white', backgroundColor: 'red', padding: '6px 20px', borderRadius: '15px',
                   cursor: 'pointer', float: 'right'}}>
             Sign Out
           </button>
@@ -38,16 +38,40 @@ const Dashboard = () => {
 
         <Link to="/presetcreator">
           <button
-          style={{color: 'black', backgroundColor: 'lightblue', padding: '6px 20px', borderRadius: '20px',
-          cursor: 'pointer'}}>
+          style={{color: 'black', backgroundColor: 'lightblue', padding: '6px 20px', borderRadius: '15px',
+                  cursor: 'pointer'}}>
             Create Scoreboard Preset</button>
         </Link>
         
         <button onClick={handleopenSessionStart}
-        style={{color: 'black', backgroundColor: 'lightgreen', padding: '6px 20px', borderRadius: '20px',
-        cursor: 'pointer'}}>
+        style={{color: 'black', backgroundColor: 'lightgreen', padding: '6px 20px', borderRadius: '15px',
+                cursor: 'pointer'}}>
           Start Scorekeeping Session
         </button>
+        <div className="user_presets"
+        style={{textAlign: "center"}}>
+          <h3>Saved Presets (placeholder)</h3>
+
+          {/* add source for image within the buttons */}
+          <button
+          style={{color: "black", backgroundColor: "white", border: '4px solid black', borderRadius: '15px',
+                  width: '320px', height:'180px'}}>
+            1
+          </button>
+          
+          <button
+          style={{color: "black", backgroundColor: "white", border: '4px solid black', borderRadius: '15px',
+                  width: '320px', height:'180px'}}>
+            2
+          </button>
+
+          <button
+          style={{color: "black", backgroundColor: "white", border: '4px solid black', borderRadius: '15px',
+                  width: '320px', height:'180px'}}>
+            3
+          </button>
+
+        </div>
 
         {ShowSessionStart && <SessionStart onClose={handleCloseSessionStart} />}
       </div>
