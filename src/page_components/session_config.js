65c14e380cddb340b6461cff42c0914833d9ff14
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2023-10-29 21:53:28 -0700

    moved dashboard css to external style sheet

--- src/page_components/session_config.js
+++ src/page_components/session_config.js
@@ -1,6 +1,7 @@
 // this component is rendered when a scorekeeping session is started,
 // this is where the user inputs their pre-session preferences
 import React from "react";
+import "../css/dashboard.css";
 
 function OpenSession() {
   window.open("/myscoreboard", "_blank");
@@ -10,10 +11,12 @@ function OpenSession() {
 function SessionStart({ onClose }) {
   return (
     <>
-      <div>
-        <h1>option to choose session, directly stream it etc.</h1>
-        <button onClick={OpenSession}>Create Session</button>
-        <button onClick={onClose}>Close Component</button>
+      <div className="SCcontain">
+        <div className="SessionCreator">
+          <h1>option to choose session, directly stream it etc.</h1>
+          <button onClick={OpenSession}>Create Session</button>
+          <button onClick={onClose}>Close Component</button>
+        </div>
       </div>
     </>
   );
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Auth } from "aws-amplify";
 import { Link } from "react-router-dom";
 import SessionStart from "../page_components/session_config";
+import "../css/dashboard.css";
 
 async function signOut() {
   try {
@@ -25,52 +26,33 @@ const Dashboard = () => {
   return (
     <>
       <div className="dash_body">
-        <h1 style={{textAlign: 'center'}}>Welcome to the dashboard!</h1>
-        
+        <h1>Welcome to the dashboard!</h1>
+
         <Link to="/">
-          <button 
-          onClick={signOut} 
-          style={{color: 'white', backgroundColor: 'red', padding: '6px 20px', borderRadius: '15px',
-                  cursor: 'pointer', float: 'right'}}>
+          <button onClick={signOut} className="SObutton">
             Sign Out
           </button>
         </Link>
 
         <Link to="/presetcreator">
-          <button
-          style={{color: 'black', backgroundColor: 'lightblue', padding: '6px 20px', borderRadius: '15px',
-                  cursor: 'pointer'}}>
-            Create Scoreboard Preset</button>
+          <button className="PresetCreatorButton">
+            Create Scoreboard Preset
+          </button>
         </Link>
-        
-        <button onClick={handleopenSessionStart}
-        style={{color: 'black', backgroundColor: 'lightgreen', padding: '6px 20px', borderRadius: '15px',
-                cursor: 'pointer'}}>
+
+        <button onClick={handleopenSessionStart} className="StartSessionButton">
           Start Scorekeeping Session
         </button>
-        <div className="user_presets"
-        style={{textAlign: "center"}}>
+
+        <div className="user_presets">
           <h3>Saved Presets (placeholder)</h3>
 
           {/* add source for image within the buttons */}
-          <button
-          style={{color: "black", backgroundColor: "white", border: '4px solid black', borderRadius: '15px',
-                  width: '320px', height:'180px'}}>
-            1
-          </button>
-          
-          <button
-          style={{color: "black", backgroundColor: "white", border: '4px solid black', borderRadius: '15px',
-                  width: '320px', height:'180px'}}>
-            2
-          </button>
+          <button className="PresetTile">1</button>
 
-          <button
-          style={{color: "black", backgroundColor: "white", border: '4px solid black', borderRadius: '15px',
-                  width: '320px', height:'180px'}}>
-            3
-          </button>
+          <button className="PresetTile">2</button>
 
+          <button className="PresetTile">3</button>
         </div>
 
         {ShowSessionStart && <SessionStart onClose={handleCloseSessionStart} />}
