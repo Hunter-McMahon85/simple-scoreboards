5ba3e14a01b72c8b4bc786f0198ecaf51efae79f
Quinn Rainen <41759774+Quinn-Rainen@users.noreply.github.com>
2023-10-30 17:36:14 -0700

    Home Page Redesign
    Slight modifications for home page v2

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,7 +10,7 @@ const landing = () => {
           <h1>Simple Scoreboards</h1>
           <form>
             <Link to="/mydashboard">
-              <button>Log in</button>
+              <button className="broadcast-button">Log in</button>
             </Link>
           </form>
           <h6>
@@ -18,38 +18,41 @@ const landing = () => {
           </h6>
         </div>
         <div class="right">
-          <p>
+          <p class="generic">
             Every great play has a great call, make your broadcast more
             proffesional with our free and easy to use overlays today!
           </p>
           <Link to="/usingsimplescoreboards">
-            <button>Learn to use Simple Scoreboard Overlays</button>
+            <button className="broadcast-button">Learn to use Simple Scoreboard Overlays</button>
           </Link>
         </div>
       </div>
-      <div class="about">
-        <h1>About</h1>
-        <p>
-          Whether its for your hobby or education, simple scoreboards provides
-          you with everything you need to get started with sports broadcasting.
-          We offer both tools and educational resources on how to create your
-          own sports broadcast!
-        </p>
-        <Link to="/learntobroadcast">
-          <button>Learn How To Become a Sports Broadcaster Here</button>
-        </Link>
-        <h2>Sports with Simple Scoreboard Overlays:</h2>
-        <ul>
-          <li>American Football</li>
-          <li>Basketball</li>
-          <li>Baseball/Softball</li>
-          <li>Soccer</li>
-          <li>Hockey</li>
-          <li>Volleyball</li>
-          <li>Rugby</li>
-          <li>Cricket</li>
-        </ul>
-      </div>
+      <div class="full-width-container">
+        <div class="about">
+          <h1>About</h1>
+          <p class="about-desc">
+            Whether its for your hobby or education, simple scoreboards provides
+            you with everything you need to get started with sports broadcasting.
+            We offer both tools and educational resources on how to create your
+            own sports broadcast!
+          </p>
+          <Link to="/learntobroadcast">
+            <button className="broadcast-button">Learn How To Become a Sports Broadcaster Here</button>
+          </Link>
+          <h2>Sports with Simple Scoreboard Overlays:</h2>
+          <ul>
+            <li>American Football</li>
+            <li>Basketball</li>
+            <li>Baseball/Softball</li>
+            <li>Soccer</li>
+            <li>Hockey</li>
+            <li>Volleyball</li>
+            <li>Rugby</li>
+            <li>Cricket</li>
+          </ul>
+        </div>
+    
+          </div>
     </>
   );
 };
